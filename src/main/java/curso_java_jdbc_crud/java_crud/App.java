746bc8b232cd3fe282f5ebcefb97bb68dc03f6ee
Clessio Silva <clessiosilva44@gmail.com>
2023-01-28 09:46:52 -0300

    primeiro commit

---
+++ src/main/java/curso_java_jdbc_crud/java_crud/App.java
@@ -0,0 +1,13 @@
+package curso_java_jdbc_crud.java_crud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
